96efaddd99829eab87371f7b6775c0979d89fd22
ArchSplash <85456939+ArchSplash@users.noreply.github.com>
2021-06-13 23:31:26 +0200

    Add files via upload

---
+++ C#/Kennwortprüfung/Kennwortprüfung/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Kennwortprüfung
+{
+    class MainClass
+    {
+        static string benutzername = "Wilhelm";
+        static string kennwort = "Raabe";
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Benutzername: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Kennwort: ");
+            string passwort = Console.ReadLine();
+
+            if (username == benutzername && kennwort == passwort)
+            {
+                Console.WriteLine("Richtig");
+            } else
+            {
+                Console.WriteLine("Falsch");
+            }
+        }
+    }
+}
---
+++ Python/Kennwortprüfung/Kennwortprüfung.py
@@ -0,0 +1,12 @@
+benutzername = "Wilhelm"
+kennwort = "Raabe"
+
+
+username = input("Benutzername: ")
+passwort = input("Kennwort: ")
+
+if username == benutzername and passwort == kennwort:
+    print("Richtig")
+
+else:
+    print("Falsch")
